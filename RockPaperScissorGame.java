de88fc1a846761c3dae33485131eeff867b40b31
Joey <jvalencia002@hotmail.com>
2016-03-23 20:44:20 -0700

    Added files via upload

--- RockPaperScissorGame.java
+++ RockPaperScissorGame.java
@@ -2,54 +2,51 @@ package rockpaperscissorgame;
 
 import java.util.Random;
 import java.util.Scanner;
-import javax.swing.*;
+import javax.swing.JOptionPane;
 public class RockPaperScissorGame 
 {
 public static void main(String[] args) 
 {
     String inputStr;
-    String personPlay="";    
-    String computerPlay="1,2,3";     
-    int computerInt;
+    String userPlay="";    
+    String compPlay="0,1,2";     
+   
    Scanner input = new Scanner(System.in);
     Random generator = new Random();
+            inputStr = JOptionPane.showInputDialog("Lets play Rock Paper Scissors! \nEnter 0 for rock \nEnter 1 for paper \nEnter 2 for scissors \nEnter 3 to quit");
+            userPlay = input.next(inputStr);
 
-
-            inputStr = JOptionPane.showInputDialog("Lets play Rock Paper Scissors! \nEnter 1 for rock \nEnter 2 for paper \nEnter 3 for scissors \nEnter 4 to quit");
-            personPlay = input.next(inputStr);
-
-            switch (computerInt = 0)
+            
             {
                 }
             do
             {
-                if (personPlay.equals(computerPlay))  
-                    JOptionPane.showMessageDialog(null, "It's a TIE! ", "TIE!", JOptionPane.INFORMATION_MESSAGE);
-
-                else if (personPlay.equals("1"))
+                if (userPlay.equals(compPlay))  
+                    JOptionPane.showMessageDialog(null, "It's a TIE! ", "Tied Game",JOptionPane.INFORMATION_MESSAGE);
+                else if (userPlay.equals("0"))
                     {
-                    if (computerPlay.equals("3"))
-                    JOptionPane.showMessageDialog(null, "Rock beats Scissors. \nYOU WIN! ", "YOU WIN!", JOptionPane.INFORMATION_MESSAGE);
+                    if (compPlay.equals("2"))
+                    JOptionPane.showMessageDialog(null, "Rock beats Scissors. \nYOU WIN! ", "User Wins!", JOptionPane.INFORMATION_MESSAGE);
                     else
-                    JOptionPane.showMessageDialog(null, "Paper beats Rock. \nYOU LOSE! ", "YOU LOSE!", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Paper beats Rock. \nYOU LOSE! ", "User Lose!", JOptionPane.INFORMATION_MESSAGE);
                     }
-                else if (personPlay.equals("2"))
+                else if (userPlay.equals("1"))
                     {
-                    if (computerPlay.equals("3"))
-                    JOptionPane.showMessageDialog(null, "Scissor beats Paper. \nYOU LOSE!", "YOU LOSE!", JOptionPane.INFORMATION_MESSAGE);
+                    if (compPlay.equals("2"))
+                    JOptionPane.showMessageDialog(null, "Scissor beats Paper. \nYOU LOSE!", "User Lose!", JOptionPane.INFORMATION_MESSAGE);
                     else
-                    JOptionPane.showMessageDialog(null, "Paper beats Rock. \nYOU WIN! ", "YOU WIN!", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Paper beats Rock. \nYOU WIN! ", "User Win!", JOptionPane.INFORMATION_MESSAGE);
                     }
-                else if (personPlay.equals("3"))
+                else if (userPlay.equals("2"))
                     {
-                     if (computerPlay.equals("1"))
-                    JOptionPane.showMessageDialog(null, "Scissor beats Paper. \nYOU WIN!", "YOU WIN!", JOptionPane.INFORMATION_MESSAGE);
+                     if (compPlay.equals("0"))
+                    JOptionPane.showMessageDialog(null, "Scissor beats Paper. \nYOU WIN!", "User Win!", JOptionPane.INFORMATION_MESSAGE);
                      else
-                    JOptionPane.showMessageDialog(null, "Rock beats Scissors. \nYOU LOSE! ", "YOU LOSE!", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Rock beats Scissors. \nYOU LOSE! ", "User Lose!", JOptionPane.INFORMATION_MESSAGE);
                     }
-                else if (personPlay.equals("4"))
+                else if (userPlay.equals("3"))
                     {
-                    JOptionPane.showMessageDialog(null, "GOOD BYE", " BYE!", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "See Yah!", " Until Next Time!", JOptionPane.INFORMATION_MESSAGE);
                     }
             }while(true);
             }   
